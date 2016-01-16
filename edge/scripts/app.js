4b2c45965a8609b088e942c8e6ec27f62c219994
Shawn Bow <zhen.bao@acadine.com>
2016-01-16 22:52:40 +0800

    Initial version
    Signed-off-by: Shawn Bow <zhen.bao@acadine.com>

---
+++ edge/scripts/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Shawn on 5/6/14.
+ */
+
+(function () {
+
+// The app entry: just after document load finished.
+    var appContainerFrame = null;
+    function init() {
+        appContainerFrame = document.getElementById('app_container');
+        appContainerFrame.src = "http://static.egret-labs.org/h5game/jianpan/v3/4/index.html";
+        appContainerFrame.focus();
+    }
+
+    window.onload  = function () {
+      init();
+    }
+})();
